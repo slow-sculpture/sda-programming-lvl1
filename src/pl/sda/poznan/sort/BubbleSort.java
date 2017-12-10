af07b17e1da6825e3c95a586536f18fb16558ce5
ChristianR <christian.rogowski@yahoo.pl>
2017-12-10 12:06:42 +0100

    BubbleSort - sorting String[] added

--- src/pl/sda/poznan/sort/BubbleSort.java
+++ src/pl/sda/poznan/sort/BubbleSort.java
@@ -10,13 +10,30 @@ public class BubbleSort {
     public static void sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 1; j < array.length; j++) {
-                if (array[j] < array[j-1]){
+                if (array[j] < array[j - 1]) {
                     ///////  > ///// - to uszereguje od max do min
                     //zamien elementy tablicy
                     //zmienna pomocnicza przechowujaca wartosc
                     int helper = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = helper;
+                    array[j] = array[j - 1];
+                    array[j - 1] = helper;
+                }
+            }
+        }
+    }
+
+    public static void sortString(String[] array) {
+//        String s1 = "ala";
+//        String s2 = "pies";
+//        int result = s1.compareTo(s2);
+        //jezeli sa rowne zwroci 0, w przeciwnym przypadku <0 lub >0 (< >)
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                int result = array[j].compareTo(array[j - 1]);
+                if (result < 0) {
+                    String helper = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = helper;
                 }
             }
         }
--- src/pl/sda/poznan/sort/BubbleSortTest.java
+++ src/pl/sda/poznan/sort/BubbleSortTest.java
@@ -10,7 +10,24 @@ public class BubbleSortTest {
         int[] array = {9, 5, 7, 12, 3};
         BubbleSort.sort(array);
         int[] expectedArrayAfterSorting = {3, 5, 7, 9, 12};
-        assertArrayEquals(expectedArrayAfterSorting , array);
+        assertArrayEquals(expectedArrayAfterSorting, array);
+    }
+
+    @Test
+    public void shouldCompareStrings() {
+        String s1 = "Da";
+        String s2 = "Df";
+        int result = s1.compareTo(s2);
+        System.out.println(result);
+
+    }
+
+    @Test
+    public void shouldSortArrayOfStrting() {
+        String[] array = {"b", "r", "a", "k"};
+        BubbleSort.sortString(array);
+        String[] expectedArrayAfterSorting = {"a", "b", "k", "r"};
+        assertArrayEquals(expectedArrayAfterSorting, array);
     }
 
 }
